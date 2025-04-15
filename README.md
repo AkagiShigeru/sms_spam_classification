28602c6a81cf8b2d2461a6f2e74cb4ba56c5520c
Alex S. <alex.transporter@gmail.com>
2025-04-15 14:38:20 +0200

    Added some more plots & better LLM example.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # sms_spam_classification
+
+This repository includes code and a small EDA to classify SMS messages into spam or ham (not spam).
+
+The dataset is publicly available under https://archive.ics.uci.edu/dataset/228/sms+spam+collection.
+
+To use this code, please install the defined poetry environment with
+`poetry install`
+
+For development purposes, please consider to set-up the pre-commit hook to ensure committing clean & formatted code.
+Please execute
+`poetry run pre-commit install`
+after executing
+`poetry install --dev`.
--- sms_classification/utils.py
+++ sms_classification/utils.py
@@ -1,6 +1,11 @@
+import numpy as np
+import pandas as pd
+import seaborn as sb
+from matplotlib import pyplot as plt
 from sklearn.base import ClassifierMixin
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.inspection import permutation_importance
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
@@ -13,6 +18,8 @@ def create_pipeline(
 
     Args:
         model_type: selected ML model
+        numerical_features: list of numerical feature names
+        text_feature: name of text feature to encode
 
     Returns:
         Pipeline object
@@ -34,11 +41,87 @@ def create_pipeline(
         ]
     )
 
-    pipeline = Pipeline(
-        [
-            ("preprocessor", preprocessor), 
-            ("classifier", model)
-        ]
-    )
+    pipeline = Pipeline([("preprocessor", preprocessor), ("classifier", model)])
 
     return pipeline
+
+
+def plot_confusion_matrix(
+    conf_matrix: np.array,
+    labels: list[str] = ["legitimate", "spam"],
+    figsize: tuple = (10, 8),
+):
+    """
+    Plot confusion matrix
+
+    Args:
+        conf_matrix: Confusion matrix array
+        labels: List of class labels
+    """
+    plt.figure(figsize=(10, 8))
+
+    sb.heatmap(
+        conf_matrix,
+        annot=True,
+        fmt="d",
+        cmap="YlOrRd",
+        xticklabels=labels,
+        yticklabels=labels,
+    )
+    plt.title("Confusion Matrix")
+    plt.xlabel("Prediction")
+    plt.ylabel("True label")
+
+    plt.tight_layout()
+
+    plt.show()
+
+
+def get_feature_importance(
+    pipeline: Pipeline,
+    numerical_features: list[str],
+    text_feature: str,
+    X_test: pd.DataFrame,
+    y_test: pd.Series,
+) -> pd.DataFrame:
+    """
+    Calculate permutation importances
+
+    Args:
+        pipeline: Fitted pipeline
+        X_test: Test features
+        y_test: Test labels
+
+    Returns:
+        DataFrame: Feature names and their importance scores
+    """
+    # Get the feature names from the preprocessor
+    feature_names = []
+    feature_names.extend(
+        pipeline.named_steps["preprocessor"][
+            text_feature
+        ].get_feature_names_out()
+    )
+    feature_names.extend(numerical_features)
+
+    X_test_trafo = pipeline.named_steps["preprocessor"].transform(X_test)
+
+    # Convert sparse matrix to dense if necessary
+    if hasattr(X_test_trafo, "toarray"):
+        X_test_trafo = X_test_trafo.toarray()
+
+    results = permutation_importance(
+        pipeline.named_steps["classifier"],
+        X_test_trafo,
+        y_test,
+        n_repeats=5,
+        random_state=42,
+    )
+
+    importances = {
+        "feature": feature_names,
+        "importance": results.importances_mean,
+        "std": results.importances_std,
+    }
+
+    return pd.DataFrame(importances)
